2f3f918e70c85d91dd0da7093fd063aa2ba4da9c
dscneto <dscneto1998@gmail.com>
2022-08-23 22:03:18 -0300

    up

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,33 @@
 import './App.css'
 
 function App() {
-  return <h1>Pietrindzn</h1>
+  return(
+    <div>
+      <div>
+        <h1>Home</h1>
+      </div>
+
+      <section>
+        <h4>Sobre</h4>
+      </section>
+
+      <section>
+        <h4>Serviços</h4>
+      </section>
+
+      <section>
+        <h4>Portifolio</h4>
+      </section>
+
+      <section>
+        <h4>Parcerias</h4>
+      </section>
+
+      <section>
+        <h4>Contato</h4>
+      </section>
+    </div>
+  )
 }
 
 export default App
