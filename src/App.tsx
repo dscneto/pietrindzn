448647cce6f703d087b77775c370a4b1884708a3
dscneto <dscneto1998@gmail.com>
2022-08-24 22:31:46 -0300

    2

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import './App.css'
 function App() {
   return(
     <div>
-      <div class="navbar">
+      <div class='navbar'>
         <h1>Home</h1>
         <div>
           <a href="#about">Sobre</a>
@@ -19,7 +19,6 @@ function App() {
 
         <p>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ex magna, iaculis nec tincidunt in, iaculis sed justo. Etiam elementum nisi vitae est aliquet elementum. Sed nec placerat felis. Mauris porta elit a gravida condimentum. Fusce nec suscipit purus, at egestas est. Mauris ultrices sodales risus. Sed hendrerit non neque at facilisis.
-
           Aliquam erat volutpat. Sed tempus condimentum quam a molestie. Aenean sit amet tempor mauris. Nulla quis rutrum urna. Sed vitae convallis dui, eget sagittis odio. Vivamus nec suscipit erat. Aenean id erat leo. Sed vel nisl quis enim tempor finibus. Nulla sed dictum massa. Fusce sollicitudin congue ullamcorper. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Ut pellentesque, quam in faucibus hendrerit, nunc erat imperdiet arcu, eget maximus massa nisl quis dui. Vivamus urna urna, consectetur vitae mollis eget, finibus mollis justo. Maecenas in tortor vel mi cursus condimentum vitae non magna. Proin eget orci nisi. Morbi commodo pulvinar lobortis.
         </p>
       </section>
